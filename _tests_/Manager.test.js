4e70202a28beba9b1fb5137892a567aab22b5bb6
krod2016 <katrinajrodriguez1993@gmail.com>
2022-01-03 21:00:33 -0600

    added lib files, updated index.js

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,15 +1,40 @@
 const Manager = require('./Manager');
 
-//create Manager card
-test ('Manager info', () => {
-  const Manager = new Manager ('Katrina', 001, 'katrinalaw@gmail.com', 1);
-  expect(employee.number).toEqual(expect.anyNumber);
+//create manager card (name, manager id, email, office number)
+test ('manager info', () => {
+  const manager = new manager ('Katrina', 001, 'katrinalaw@gmail.com', 1);
+  expect(manager.name).toEqual(expect.any(String));
+  expect(manager.id).toEqual(expect.any(Number));
+  expect(manager.email).toEqual(expect.any(String));
 });
 
-// getRole coding starts here
-test('gets employee', () => {
-    const manager = new Manager ('Katrina', 001, 'katrinalaw@gmail.com', 1);
+//getId coding here
+test('manager ID', () => {
+  const manager = new manager ('Katrina', 001, 'katrinalaw@gmail.com', 1);
+  expect(manager.getId()).toEqual(expect.any(Number));
+});
 
+//getemail coding here
+test('manager email', () => {
+  const manager = new manager ('Katrina', 001, 'katrinalaw@gmail.com', 1);
+  expect(manager.getEmail()).toEqual(expect.stringContaining(manager.email.toString()));
+});
+
+//getName coding here
+test('manager name', () => {
+  const manager = new manager ('Katrina', 001, 'katrinalaw@gmail.com', 1);
+  expect(manager.getName()).toEqual(expect.any(String));
+});
+
+//getRole coding here
+test('manager role', () => {
+  const manager = new manager ('Katrina', 001, 'katrinalaw@gmail.com', 1);
+  expect(manager.getRole()).toEqual("manager");
+}); 
+
+// getRole coding starts here
+test('gets manager', () => {
+    const manager = new Manager ('Katrina', 001, 'katrinalaw@gmail.com', 1);
     expect(manager.getRole()).toEqual("Manager");
 }); 
 
