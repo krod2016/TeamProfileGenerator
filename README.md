c486bdd5648369af336cfb50a50ddaffbea3c18b
krod2016 <katrinajrodriguez1993@gmail.com>
2022-01-09 14:46:34 -0600

    updated index.html page

--- README.md
+++ README.md
@@ -12,14 +12,14 @@ This team profile generator allows managers to input their employees information
 * Node.js
 
 # Installation
-To begin the coding, I downloaded node, and inquirer.
-Afer coding was completed, I typed "index.js" to make sure the code was working properly.
+* To begin the coding, I downloaded node, and inquirer.
+* Afer coding was completed, I typed "index.js" to make sure the code was working properly.
 
 # Process
-First, I made tests for the manager, employee, engineer, and intern to see if my coding was successful.
-After testing, I made pages for each person to conduct the proper coding, so it could be dynamically shown on the page.
-Once all the pages were made, I made an html outline so the JavaScript coding could be dynamically shown.
-Then, I added CSS to the website to for finishing touches.
+* First, I made tests for the manager, employee, engineer, and intern to see if my coding was successful.
+* After testing, I made pages for each person to conduct the proper coding, so it could be dynamically shown on the page.
+* Once all the pages were made, I made an html outline so the JavaScript coding could be dynamically shown.
+* Then, I added CSS to the website to for finishing touches.
 
 # Credits
 Made with love by Katrina Rodriguez
--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,7 +2,7 @@ const Intern = require('./Intern');
 
 //create intern card (name, intern id, email, office number)
 test ('intern info', () => {
-  const intern = new intern ('Eva', 004, 'ebena234@yahoo.com', 'USU');
+  const intern = new intern ('Eva', 005, 'ebena234@yahoo.com', 'USU');
   expect(intern.name).toEqual(expect.any(String));
   expect(intern.id).toEqual(expect.any(Number));
   expect(intern.email).toEqual(expect.any(String));
@@ -10,31 +10,31 @@ test ('intern info', () => {
 
 //getId coding here
 test('intern ID', () => {
-  const intern = new intern ('Eva', 004, 'ebena234@yahoo.com', 'USU');
+  const intern = new intern ('Eva', 005, 'ebena234@yahoo.com', 'USU');
   expect(intern.getId()).toEqual(expect.any(Number));
 });
 
 //getemail coding here
 test('intern email', () => {
-  const intern = new intern ('Eva', 004, 'ebena234@yahoo.com', 'USU');
+  const intern = new intern ('Eva', 005, 'ebena234@yahoo.com', 'USU');
   expect(intern.getEmail()).toEqual(expect.stringContaining(intern.email.toString()));
 });
 
 //getName coding here
 test('intern name', () => {
-  const intern = new intern ('Eva', 004, 'ebena234@yahoo.com', 'USU');
+  const intern = new intern ('Eva', 005, 'ebena234@yahoo.com', 'USU');
   expect(intern.getName()).toEqual(expect.any(String));
 });
 
 // getSchool coding here
 test('gets intern school', () => {
-  const intern = new Intern ('Eva', 004, 'ebena234@yahoo.com', 'USU');
+  const intern = new Intern ('Eva', 005, 'ebena234@yahoo.com', 'USU');
   expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
 
 // getRole coding here
 test('gets intern role', () => {
-  const intern = new Intern ('Eva', 004, 'ebena234@yahoo.com', 'USU');
+  const intern = new Intern ('Eva', 005, 'ebena234@yahoo.com', 'USU');
   expect(intern.getRole()).toEqual("Intern");
 });
 
